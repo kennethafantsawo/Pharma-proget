8c0b60b2b383a36078f0122306b2b7930e056a60
kenneth afantsawo <kennethafantsawo@gmail.com>
2025-07-03 23:46:10 +0000

    je ne voit rien a installer

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: false,
   runtimeCaching: [
     {
       urlPattern: ({ url }) => {
