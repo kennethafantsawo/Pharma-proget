42205f2a1160b5d2b8c9a6b76e4c7f34b556a750
kenneth afantsawo <kennethafantsawo@gmail.com>
2025-07-04 00:28:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: false,
+  disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
     {
       urlPattern: ({ url }) => {
